e91f0ca0834f47720f61a5d8da92bc7583d79721
Rebelan <100076303+Rebelan@users.noreply.github.com>
2026-03-25 12:32:52 +0100

    internacionalización lógica
    Está a medias en general porque traducir todo el texto en una de prueba..

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,22 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(),]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideHttpClient(),
+    
+    provideTranslateService({
+      lang: 'es',
+      fallbackLang: 'es',
+      loader: provideTranslateHttpLoader({
+        prefix: '/i18n/',
+        suffix: '.json'
+      })
+    })
+  ]
 };
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, CommonModule, RouterLinkActive, TranslatePipe],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+
+  private translate = inject(TranslateService);
+  
+  currentLang = signal('es');
+
+  constructor() {
+    this.translate.addLangs(['es','en']);
+    this.translate.setFallbackLang('es').subscribe();
+
+    const savedLang = localStorage.getItem('lang');
+    const browserLang = this.translate.getBrowserLang();
+    const langToUse = 
+      savedLang && ['es', 'en'].includes(savedLang)
+        ? savedLang
+        : (browserLang && ['es', 'en'].includes(browserLang) ? browserLang : 'es');
+
+    this.changeLanguage(langToUse);
+  }
+
+  changeLanguage(lang: string): void {
+    this.translate.use(lang).subscribe(() =>{
+      this,this.currentLang.set(lang);
+      localStorage.setItem('lang', lang);
+    });
+      
+  }
 
 }
