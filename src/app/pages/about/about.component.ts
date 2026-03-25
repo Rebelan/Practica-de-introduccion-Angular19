90cadf5cee10d76d294ca6d30999fe836d05832e
Rebelan <100076303+Rebelan@users.noreply.github.com>
2026-03-25 11:27:06 +0100

    Filtros para la tabla con signals

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { AboutApiService, User } from '../../services/about-api.service';
 import { Router } from '@angular/router';
 
@@ -14,22 +14,53 @@ export class AboutComponent implements OnInit {
   private aboutApiService = inject(AboutApiService);
   private router = inject(Router);
 
-  users: User[] = [];
-  loading = true;
-  error = '';
+  users = signal<User[]>([])
+  loading = signal(true);
+  error = signal('');
+
+  // filters
+  nameFilter = signal('');
+  cityFilter = signal('');
 
   ngOnInit(): void {
     this.aboutApiService.getUsers().subscribe({
       next: (data) => {
-        this.users = data;
-        this.loading = false;
+        this.users.set(data)
+        this.loading.set(false);
       },
       error: () => {
-        this.error = 'No se pudieron cargar los usuarios';
-        this.loading = false;
+        this.error.set('No se pudieron cargar los usuarios');
+        this.loading.set(false);
       }
     });
   }
+
+  cities = computed(() => {
+    const uniqueCities = this.users().map(user => user.address.city);
+    return [...new Set(uniqueCities)].sort();
+  });
+
+  filteredUsers = computed(() => {
+    const name = this.nameFilter().trim().toLowerCase();
+    const city = this.cityFilter();
+
+    return this.users().filter(user => {
+      const matchesName =
+        !name || user.name.toLowerCase().includes(name) ||
+        user.username.toLowerCase().includes(name);
+      
+      const matchesCity =
+        !city || user.address.city === city;
+      return matchesName && matchesCity;
+    });
+  });
+
+
+  resetFilters(): void {
+    this.nameFilter.set('');
+    this.cityFilter.set('');
+  }
+
 
   goToDetail(id: number): void {
     this.router.navigate(['/about', id]);
